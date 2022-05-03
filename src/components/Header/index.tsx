31a529df2a114dbbbf981cc11f692025ae1e14e3
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-05-02 22:28:06 -0300

    project init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderContainer } from "./style";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <div>teste</div>
+      <div>teste</div>
+      <div>teste</div>
+    </HeaderContainer>
+  );
+}
---
+++ src/components/Header/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+`;
